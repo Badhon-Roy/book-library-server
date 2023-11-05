2cb8afd9e2f76de466cb1221f7086496506ffbb4
Badhon-Roy <roybadhon286@gmail.com>
2023-11-05 13:56:58 +0600

    specefic book quentity is update

--- index.js
+++ index.js
@@ -29,24 +29,34 @@ async function run() {
     const bookCollection = client.db("bookDB").collection("books");
     await client.connect();
     app.get('/categories', async (req, res) => {
-        const result = await categoryCollection.find().toArray();
-        res.send(result)
+      const result = await categoryCollection.find().toArray();
+      res.send(result)
     })
     app.get('/allBooks', async (req, res) => {
-        const result = await bookCollection.find().toArray();
-        res.send(result)
+      const result = await bookCollection.find().toArray();
+      res.send(result)
     })
     app.get('/books/:category', async (req, res) => {
-        const { category } = req.params;
-        const result = await bookCollection.find({ category: category }).toArray();
-        res.send(result);
+      const { category } = req.params;
+      const result = await bookCollection.find({ category: category }).toArray();
+      res.send(result);
     });
     app.get('/allBooks/:id', async (req, res) => {
-        const id = req.params.id;
-        const query = { _id: new ObjectId(id) }
-        const result = await bookCollection.findOne(query)
-        res.send(result)
+      const id = req.params.id;
+      const query = { _id: new ObjectId(id) }
+      const result = await bookCollection.findOne(query)
+      res.send(result)
     })
+    app.put('/allBooks/:id', async (req, res) => {
+      const id = req.params.id;
+      const query = { _id: new ObjectId(id) }
+      const book = await bookCollection.updateOne(
+        query,
+        { $inc: { quantity: -1 } },
+        { new: true }
+      );
+      res.json(book);
+    });
 
 
 
@@ -67,9 +77,9 @@ run().catch(console.dir);
 
 // Sample route and controller
 app.get('/', (req, res) => {
-    res.send('book library API is running');
+  res.send('book library API is running');
 });
 
 app.listen(port, () => {
-    console.log(`book library API is running on port: ${port}`);
+  console.log(`book library API is running on port: ${port}`);
 });
